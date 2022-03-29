eb50c1e8d70e0c774b3fa378daea0143de0481b7
Daniel Leone <danielleone60@gmail.com>
2022-03-29 13:43:14 -0300

    carrito funcional en consola

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # commit #1 hice las bases de los productos y el carrito
 
 ### agregué el archivo .md
+
+# commit #2 agregué los productos de manera dinámica
+
+# commit #3 cree el carrito funcional en consola, solo modificando la cantidad y no el precio por ahora.
--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const fragment = document.createDocumentFragment();
+const fragmentProduct = document.createDocumentFragment();
+
+const fragmentCart = document.createDocumentFragment();
 
 const fruit = document.querySelector('.individualFruit');
 
-const fruitsContainer = document.querySelector('.fruitsContainer')
+const fruitsContainer = document.querySelector('.fruitsContainer');
+
+const cart = []
 
 const fruits = [
     {
+        id: 1,
         name : 'Banana 🍌',
         price : 300,
         quantity : 1
     },
 
     {
+        id: 2,
         name : 'Manzana 🍏',
         price : 100,
         quantity : 1
     },
 
     {
+        id: 3,
         name : 'Sandía 🍉',
         price : 200,
         quantity : 1
@@ -28,9 +35,23 @@ fruits.forEach( (f)=>{
     const clone = fruit.content.firstElementChild.cloneNode(true);
 
     clone.querySelector('.fruitName').textContent = f.name;
-    clone.querySelector('.fruitPrice').textContent = f.price;
+    clone.querySelector('.fruitPrice').textContent = '$' + f.price;
 
-    fragment.appendChild(clone)
+    clone.querySelector('button').addEventListener('click', ()=>{
+        console.log(f.name + 'ejecutado');
+
+        const index = cart.findIndex( (item) => item.id === f.id );
+
+        if(index === -1){
+            cart.push(f)
+        } else {
+            cart[index].quantity++;
+        }
+
+        console.log(cart);
+    })
+
+    fragmentProduct.appendChild(clone)
 } )
 
-fruitsContainer.appendChild(fragment)
+fruitsContainer.appendChild(fragmentProduct)
